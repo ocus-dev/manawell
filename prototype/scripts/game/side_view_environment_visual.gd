c5562fdc97421700bbe0c2e8b0360990af3e9f45
Scott Thomas <scotthomasdev@gmail.com>
2026-09-13 18:45:42 -0700

    Hide prepared environment lane

--- prototype/scripts/game/side_view_environment_visual.gd
+++ prototype/scripts/game/side_view_environment_visual.gd
@@ -8,6 +8,7 @@ const BACKDROP_PATH := "res://assets/side-view/environment/backdrop_level_1.png"
 const LANE_PATH := "res://assets/side-view/environment/lane.png"
 
 @export var use_prepared_layers := true
+@export var show_lane := false
 
 var backdrop_texture: Texture2D
 var lane_texture: Texture2D
@@ -30,7 +31,8 @@ func get_platform_rects() -> Array[Dictionary]:
 func _draw() -> void:
 	if is_prepared():
 		draw_texture_rect(backdrop_texture, Rect2(Vector2.ZERO, LOGICAL_SIZE), false)
-		draw_texture_rect(lane_texture, Rect2(0.0, GROUND_Y, LOGICAL_SIZE.x, LOGICAL_SIZE.y - GROUND_Y), false)
+		if show_lane:
+			draw_texture_rect(lane_texture, Rect2(0.0, GROUND_Y, LOGICAL_SIZE.x, LOGICAL_SIZE.y - GROUND_Y), false)
 	else:
 		_draw_debug_fallback()
 	_draw_platforms()
